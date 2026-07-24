07421d371f3158d3e6c4a91a7c5654e0ce8a4042
ikhamula <ilyahamula95@gmail.com>
2026-07-25 00:15:16 +0300

    removed old files

--- include/fittable_layout.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "frameworks_core/ControlWrapper.hpp"
-
-template <typename T>
-concept FittableLayout = requires(T layout, ControlWrapper* parent)
-{
-	layout.fitTo(parent);
-};
