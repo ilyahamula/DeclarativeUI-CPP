5ca0a05da1f5ea164e384d7edc06ddba3dc8841a
ikhamula <ilyahamula95@gmail.com>
2026-09-01 12:42:55 +0300

    cleanup the main functions

--- examples/main_imgui.cpp
+++ examples/main_imgui.cpp
@@ -3,84 +3,17 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
+#include <functional>
 
 namespace
 {
-    void DrawDemoUI()
-    {
-        std::string m_text = "Initial text";
-        float m_sliderValue = 50.5f;
-        int m_sliderValue1 = 25;
-        int m_radioChoice1 = 0;
-        int m_radioChoice = 0;
-        std::string m_selectedCombo = "Goodbye";
-        bool m_checked = false;
-        bool m_toggle = false;
-        bool showPopup = false;
-
-        std::string m_multilineText = "Type something here...";
-        std::string m_password;
-        int m_spinInt = 42;
-        float m_spinFloat = 1.5f;
-        Date m_date { .year = 2026, .month = 2, .day = 22 };
-        Time m_time { .hour = 9, .minute = 30, .second = 0 };
-        bool showControlsPopup = false;
-        bool showMessageBoxPopup = false;
-        float m_progress = 0.35f;
-        std::string m_tabNote = "Add notes here...";
-        bool m_tabLogging = false;
-        Color m_themeColor { .r = 0.26f, .g = 0.59f, .b = 0.98f, .a = 1.0f };
-        
-        drawControlsUI(m_multilineText, m_password, m_spinInt, m_spinFloat, m_date, m_time, m_toggle, m_progress, m_tabNote, m_tabLogging, m_themeColor,
-            [&showControlsPopup]() {
-                showControlsPopup = true;
-            },
-            [&showMessageBoxPopup]() {
-                showMessageBoxPopup = true;
-            }).show();
-
-        {
-            char msgBuf[512];
-            std::snprintf(msgBuf, sizeof(msgBuf),
-                "multilineText: %s\npassword: %s\nspinInt: %d\nspinFloat: %.2f\n"
-                "date: %04d-%02d-%02d\ntime: %02d:%02d:%02d",
-                m_multilineText.c_str(), m_password.c_str(), m_spinInt, m_spinFloat,
-                m_date.year, m_date.month, m_date.day,
-                m_time.hour, m_time.minute, m_time.second);
-            MessageBox("Controls State", msgBuf)
-                .withStyle(MessageBoxStyle::Info)
-                .withButtons(MessageBoxButtons::OK)
-                .show(showControlsPopup);
-        }
-
-        MessageBox("Question", "Do you like this framework?")
-            .withStyle(MessageBoxStyle::Question)
-            .withButtons(MessageBoxButtons::YesNo)
-            .show(showMessageBoxPopup);
-        
-    }
+    // Owns the GLFW/OpenGL/ImGui lifetime and the frame loop; `drawUI` is invoked
+    // once per frame between NewFrame() and Render(), so callers only describe UI.
+    void runImGuiApp(const std::function<void()>& drawUI);
 }
 
 int main(int argc, char** argv)
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // Large enough to host the auto-fit Controls Demo dialog (~832x520)
-    GLFWwindow* window = glfwCreateWindow(1000, 700, "DeclarativeUI - ImGui", nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 150");
-
     float value = 5.0f;
     bool checked = false;
 
@@ -102,33 +35,62 @@ int main(int argc, char** argv)
     int boundCount = 40;
     bool countLocked = false;
 
-    while (!glfwWindowShouldClose(window))
+    runImGuiApp([&]
     {
-        glfwPollEvents();
-
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        //DrawDemoUI();
         drawGroupBoxDisabling(deliveryName, deliveryEmail, copies, express, groupDisabled).show();
         drawStackAndTabDisabling(tabNote, level, verboseLogging, pageDisabled, panelDisabled).show();
         drawGroupDisabledByCheckBox(boundCount, countLocked).show();
+    });
 
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(window);
-    }
-
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-    glfwDestroyWindow(window);
-    glfwTerminate();
     return 0;
+}
+
+namespace
+{
+    void runImGuiApp(const std::function<void()>& drawUI)
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+        // Large enough to host the auto-fit Controls Demo dialog (~832x520)
+        GLFWwindow* window = glfwCreateWindow(1000, 700, "DeclarativeUI - ImGui", nullptr, nullptr);
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 150");
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+            drawUI();
+
+            ImGui::Render();
+            int display_w, display_h;
+            glfwGetFramebufferSize(window, &display_w, &display_h);
+            glViewport(0, 0, display_w, display_h);
+            glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+            glfwSwapBuffers(window);
+        }
+
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
 }
--- examples/main_wx.cpp
+++ examples/main_wx.cpp
@@ -3,29 +3,6 @@
 
 class DeclarativeApp : public wxApp
 {
-    std::string m_text = "Initial text";
-    float m_sliderValue = 50.5f;
-    int m_sliderValue1 = 25;
-    int m_radioChoice1 = 0;
-    int m_radioChoice = 0;
-    std::string m_selectedCombo = "Goodbye";
-    bool m_checked = false;
-    bool m_toggle = false;
-
-    std::string m_multilineText = "Type something here...";
-    std::string m_password;
-    int m_spinInt = 42;
-    float m_spinFloat = 1.5f;
-    Date m_date { .year = 2026, .month = 2, .day = 22 };
-    Time m_time { .hour = 9, .minute = 30, .second = 0 };
-    float m_progress = 0.35f;
-    std::string m_tabNote = "Add notes here...";
-    bool m_tabLogging = false;
-    Color m_themeColor { .r = 0.26f, .g = 0.59f, .b = 0.98f, .a = 1.0f };
-
-    float value = 1.0f;
-    bool checked = false;
-
     // Container-disabling demo state: members, so the bound refs outlive the
     // modeless dialogs that read them.
     std::string m_deliveryName = "Ada Lovelace";
@@ -44,46 +21,13 @@ class DeclarativeApp : public wxApp
     int m_boundCount = 40;
     bool m_countLocked = false;
 
-    void DrawUI()
-    {
-        drawControlsUI(m_multilineText, m_password, m_spinInt,
-            m_spinFloat, m_date, m_time, m_toggle, m_progress, m_tabNote, m_tabLogging, m_themeColor, [this]() {
-            MessageBox("Controls State",
-                "multilineText: " + m_multilineText + "\n"
-                "password: " + m_password + "\n"
-                "spinInt: " + std::to_string(m_spinInt) + "\n"
-                "spinFloat: " + std::to_string(m_spinFloat) + "\n"
-                "date: " + std::to_string(m_date.year) + "-"
-                         + std::to_string(m_date.month) + "-"
-                         + std::to_string(m_date.day) + "\n"
-                "time: " + std::to_string(m_time.hour) + ":"
-                         + std::to_string(m_time.minute) + ":"
-                         + std::to_string(m_time.second))
-                .withStyle(MessageBoxStyle::Info)
-                .withButtons(MessageBoxButtons::OK)
-                .show();
-        },
-        [this]() {
-            MessageBox("Question", "Do you like this framework?")
-                .withStyle(MessageBoxStyle::Question)
-                .withButtons(MessageBoxButtons::YesNo)
-                .show();
-        }).show();
-    }
-
 public:
     bool OnInit() override
     {
-        Bind(wxEVT_IDLE, &DeclarativeApp::OnIdle, this);
         drawGroupBoxDisabling(m_deliveryName, m_deliveryEmail, m_copies, m_express, m_groupDisabled).show();
         drawStackAndTabDisabling(m_tabNote2, m_level, m_verboseLogging, m_pageDisabled, m_panelDisabled).show();
         drawGroupDisabledByCheckBox(m_boundCount, m_countLocked).show();
         return true;
-    }
-
-    void OnIdle(wxIdleEvent& event)
-    {
-        wxApp::OnIdle(event);
     }
 };
 
